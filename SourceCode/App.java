e369f46234ffac72b34414b4bc92c7f463e2ae7a
dalalsaloni96 <dalalsaloni96@gmail.com>
2019-12-01 21:50:20 -0500

    App.java

---
+++ SourceCode/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run() {
+				new MainFrame();
+			}
+		});	
+	}
+}
